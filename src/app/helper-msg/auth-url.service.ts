5276396983287158b2a6be5ee233f66730ad6b2a
aman <amansainigithub>
2022-01-09 00:15:29 +0530

    LINKING

--- src/app/helper-msg/auth-url.service.ts
+++ src/app/helper-msg/auth-url.service.ts
@@ -7,7 +7,7 @@ export class AuthURLService {
 
   constructor() { }
 
-  // public authUrl:string = 'http://localhost:3355/api/admin/';
-  public authUrl:string = 'http://64.227.8.158:3355/api/admin/';
+  public authUrl:string = 'http://localhost:3355/api/admin/';
+  // public authUrl:string = 'http://64.227.8.158:3355/api/admin/';
   
 }
