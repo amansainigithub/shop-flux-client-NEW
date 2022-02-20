9bbad0c97abec600e13c941e743128e3c11c1ba8
aman <amansainigithub>
2022-02-20 21:05:19 +0530

    Url minor changes

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// const AUTH_API = 'http://64.227.8.158:3355/api/auth/';
-const AUTH_API = 'http://localhost:3355/api/auth/';
+const AUTH_API = 'http://64.227.8.158:3355/api/auth/';
+// const AUTH_API = 'http://localhost:3355/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
